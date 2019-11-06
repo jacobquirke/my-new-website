77291d77b5e034c88f3de8cda2588ac13983ebdb
Jacob Quirke <55559784+jacobquirke@users.noreply.github.com>
2019-11-06 10:08:33 +0000

    /inc

---
+++ contact-form.php
@@ -0,0 +1,14 @@
+<div id="contactForm">
+
+  <h3 class="contact-h3">Get in touch</h3>
+  <p>I'll get back to you as quickly as possible</p>
+  
+  <form action="#">
+    <input placeholder="Name" type="text" required />
+    <input placeholder="Email" type="email" required />
+    <input placeholder="Subject" type="text" required />
+    <textarea placeholder="Comment"></textarea>
+    <input class="formBtn" type="submit" />
+    <input class="formBtn" type="reset" />
+  </form>
+</div>
---
+++ header.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" type="text/css" href="external/style.css">
+<link rel="stylesheet" type="text/css" href="external/css/contact-form.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<style>
+    .fa {
+    padding: 20px;
+    font-size: 30px;
+width: 30px;
+text-align: center;
+text-decoration: none;
+border-radius: 50%;
+transition: all 250ms ease-in;
+margin-left: 2.5px;
+  margin-right: 2.5px;
+}
+</style>
+</head>
+<body>
+<header>
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a id="contact">Contact</a></li>
+</ul>
+  
+</div>
+</header>
